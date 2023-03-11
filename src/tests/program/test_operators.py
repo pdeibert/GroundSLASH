3bc2b6abc7ea92ba85608f8679a45bb90c8698b9
Philipp Deibert <pdeibert@pm.me>
2023-03-11 11:16:06 +0100

    Fix faulty operator tests

--- src/tests/program/test_operators.py
+++ src/tests/program/test_operators.py
@@ -31,12 +31,8 @@ class TestSubstitution(unittest.TestCase):
         self.assertEqual(~RelOp.UNEQUAL, RelOp.EQUAL)  # not x != y <=> x  = y
         self.assertEqual(~RelOp.LESS, RelOp.GREATER_OR_EQ)  # not x  < y <=> x >= y
         self.assertEqual(~RelOp.GREATER, RelOp.LESS_OR_EQ)  # not x  > y <=> x <= y
-        self.assertEqual(
-            ~RelOp.LESS_OR_EQ, RelOp.GREATER
-        )  # not x <= y <=> x  > y
-        self.assertEqual(
-            ~RelOp.GREATER_OR_EQ, RelOp.LESS
-        )  # not x >= y <=> x  < y
+        self.assertEqual(~RelOp.LESS_OR_EQ, RelOp.GREATER)  # not x <= y <=> x  > y
+        self.assertEqual(~RelOp.GREATER_OR_EQ, RelOp.LESS)  # not x >= y <=> x  < y
 
         # TODO: eval
 
