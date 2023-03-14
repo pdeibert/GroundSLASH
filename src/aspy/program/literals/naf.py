b25faa66609ad4a75dff824f162d347aed7de789
Philipp Deibert <pdeibert@pm.me>
2023-03-14 16:24:22 +0100

    Docstrings

--- src/aspy/program/literals/naf.py
+++ src/aspy/program/literals/naf.py
@@ -7,6 +7,19 @@ from .predicate import PredicateLiteral
 def Naf(
     literal: Union[PredicateLiteral, AggregateLiteral], value: bool = True
 ) -> Union[PredicateLiteral, AggregateLiteral]:
+    """Sets the `naf` (negation-as-failure) attribute of a given aggregate or predicate literal.
+
+    Args:
+        literal: `PredicateLiteral` or `AggregateLiteral` instance.
+        value: Boolean value to be set for the `naf` property of the literal.
+
+    Returns:
+        `PredicateLiteral` or `AggregateLiteral` instance.
+        Instances are the same as the input instances, but with the `naf` attribute set.
+
+    Raises:
+        ValueError: Literal of wrong type (checked just in case).
+    """  # noqa
     if not isinstance(literal, (PredicateLiteral, AggregateLiteral)):
         raise ValueError(
             "Negation as failure is only applicable to predicate and aggregate literals."  # noqa
--- src/aspy/program/literals/neg.py
+++ src/aspy/program/literals/neg.py
@@ -7,6 +7,19 @@ from .predicate import PredicateLiteral
 def Neg(
     literal: Union[PredicateLiteral, AggregateLiteral], value: bool = True
 ) -> Union[PredicateLiteral, AggregateLiteral]:
+    """Sets the `neg` (classical negation) attribute of a given aggregate or predicate literal.
+
+    Args:
+        literal: `PredicateLiteral` or `AggregateLiteral` instance.
+        value: Boolean value to be set for the `neg` property of the literal.
+
+    Returns:
+        `PredicateLiteral` or `AggregateLiteral` instance.
+        Instances are the same as the input instances, but with the `neg` attribute set.
+
+    Raises:
+        ValueError: Literal of wrong type (checked just in case).
+    """  # noqa
     if not isinstance(literal, PredicateLiteral):
         raise ValueError("Classical negation only applicable to predicate literals.")
 
